0ec244c663c067a8db99693b0ce1b951e41d4071
Kellen James <8484636@ups.com>
2024-11-04 15:18:15 -0500

    chore: generate angular components for project

---
+++ social-links-profile/src/app/name-location/name-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './name-location.component.html',
+  styleUrl: './name-location.component.scss'
+})
+export class NameLocationComponent {
+
+}
