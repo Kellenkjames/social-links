d802fdc37a373b66c06a9766a900d690a9d9c2e3
Kellen James <8484636@ups.com>
2024-11-05 11:18:34 -0500

    feat: add dynamic properties to card component

--- social-links-profile/src/app/card/card.component.ts
+++ social-links-profile/src/app/card/card.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { LinksComponent } from '../links/links.component';
+import { NameLocationComponent } from '../name-location/name-location.component';
+import { ProfilePicComponent } from '../profile-pic/profile-pic.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [ProfilePicComponent, NameLocationComponent, LinksComponent],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {}
+export class CardComponent {
+  imageUrl: string = 'assets/images/avatar-jessica.jpeg';
+  title: string = 'Jessica Randall';
+  description: string = 'London, United Kingdom';
+  bio: string = 'Front-end developer and avid reader';
+  links: string[] = [
+    'Github',
+    'Frontend Mentor',
+    'LinkedIn',
+    'Twitter',
+    'Instagram',
+  ];
+}
